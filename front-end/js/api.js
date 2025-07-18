aea950f827b401292b332a869146d98337fc72ca
Chris-gus <chrisgus2605@gmail.com>
2025-07-18 08:38:27 -0300

    feito javascript pra API do usuário, corrigida logo no header

---
+++ front-end/js/api.js
@@ -0,0 +1,49 @@
+function apiHandShake() {
+    const token = localStorage.getItem('token')
+    if (!token) {
+        window.location.href = 'home.html';
+    }
+    else {
+        fetch("https://localhost:7082/Auth/HandShake", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            }
+        }).then((result) => {
+            return result.json();
+        }).then((data) => {
+        }).catch((error) => {
+            localStorage.clear();
+            window.location.href = 'home.html';
+        })
+    }
+}
+
+async function getUser(route) {
+    const response = await fetch(`https://localhost:7082/${route}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        }
+    })
+    if (!response.ok) {
+        throw new Error('Erro ao buscar da api');
+    }
+    return await response.json();
+}
+
+async function apiPost(route, body) {
+    const response = await fetch(`https://localhost:7082/${route}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+        },
+        body: JSON.stringify(body)
+    })
+    if (!response.ok) {
+        throw new Error('Erro ao buscar da api');
+    }
+    return await response.json();
+}
