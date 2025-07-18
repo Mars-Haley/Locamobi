8745266e843e4339c61d5422e22cb756e3a70295
GuiAPD <GuiAPD7@gmail.com>
2025-07-18 09:14:49 -0300

    base esqueceu a senha beta

---
+++ front-end/js/login-esqueci-senha.js
@@ -0,0 +1,18 @@
+document.getElementById("forgotForm").addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+  
+    const res = await fetch("https://localhost:7082/auth/forgot-password", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email })
+    });
+  
+    const msg = await res.text();
+    document.getElementById("message").textContent = msg;
+    then((data) => {
+    localStorage.setItem('token', data.token);
+    localStorage.setItem('email', JSON.stringify(data.email));
+    window.location.href = "index.html"
+})
+  });
